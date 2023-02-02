00783d2e61ee629ae0f15cabc28fa0e411adfc5b
jeston13 <imjeston@gmail.com>
2023-02-02 11:48:04 +0530

    schema.sql file modified

---
+++ employee_service/DB/schema.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE "employee"(
+    "empId" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "age" NUMBER NOT NULL,
+    "gender" TEXT NOT NULL,
+    "status" BOOLEAN NOT NULL
+    "deptid" INTEGER
+    CONSTRAINT fk_departments
+    FOREIGN KEY (deptid)
+    REFERENCES department(deptId)
+);
+
+CREATE TABLE "department"(
+    "deptId" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "deptName" TEXT NOT NULL
+);
