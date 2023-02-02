af07992b0964b4607bd702d7ec0d4e3e17ab0fdb
jeston13 <imjeston@gmail.com>
2023-02-02 13:04:36 +0530

    schema changed

--- employee_service/DB/init.db.py
+++ employee_service/DB/init.db.py
@@ -3,3 +3,21 @@ import sqlite3
 connection = sqlite3.connect('employee_services.db')
 with open('employee_service/DB/schema.sql') as f:
     connection.executescript(f.read())
+
+cursor = connection.cursor()
+
+cursor.execute ("INSERT INTO department (deptName) VALUES (?)",
+("HR",)
+)
+cursor.execute ("INSERT INTO department (deptName) VALUES (?)",
+("DEVELOPEMENT",)
+)
+cursor.execute ("INSERT INTO department (deptName) VALUES (?)",
+("FINANCE",)
+)
+cursor.execute ("INSERT INTO department (deptName) VALUES (?)",
+("MARKETING",)
+)
+
+connection.commit()
+connection.close()
--- employee_service/DB/schema.sql
+++ employee_service/DB/schema.sql
@@ -4,14 +4,21 @@ CREATE TABLE "employee"(
     "name" TEXT NOT NULL,
     "age" NUMBER NOT NULL,
     "gender" TEXT NOT NULL,
-    "status" BOOLEAN NOT NULL,
-    "deptid" INTEGER,
-    CONSTRAINT fk_departments
-    FOREIGN KEY (deptid)
-    REFERENCES department(deptId)
+    "status" BOOLEAN NOT NULL
 );
 
 CREATE TABLE "department"(
     "deptId" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "deptName" TEXT NOT NULL
+);
+
+CREATE TABLE "employee_department"(
+    "empId" INTEGER NOT NULL,
+    "deptId" INTEGER NOT NULL,
+    CONSTRAINT fk_departments
+    FOREIGN KEY (deptId)
+    REFERENCES department(deptId)
+    CONSTRAINT fk_employee
+    FOREIGN KEY (empId)
+    REFERENCES employee(empId)
 );
